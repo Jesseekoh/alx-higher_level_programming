223a1dfd843c8ee2991097847ed7d42b571d37ae
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-08-08 19:33:33 +0000

    fix 100-print_tebahpla.py 2

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+print_upper = False
 for i in range(90, 64, -1):
-    print("{}".format(chr(i)), end="")
+    if print_upper:
+        print("{}".format(chr(i)), end="")
+        print_upper = False
+    elif not print_upper:
+        print("{}".format(chr(i + 32)), end="")
+        print_upper = True
