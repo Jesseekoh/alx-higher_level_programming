3bd5579df3366cb391321a968a89da0b055c2a09
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-08-10 20:03:17 +0000

    fix 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-arg_len = len(argv) - 1
-if arg_len == 1:
-    print("{} argument:".format(arg_len))
-elif arg_len > 2:
-    print("{} arguments:".format(arg_len))
-else:
-    print("{} arguments.".format(0))
+    arg_len = len(argv) - 1
+    if arg_len == 1:
+        print("{} argument:".format(arg_len))
+    elif arg_len > 2:
+        print("{} arguments:".format(arg_len))
+    else:
+        print("{} arguments.".format(0))
 
-for i in range(len(argv)):
-    if i > 0:
-        print("{}: {}".format(i, argv[i]))
+    for i in range(len(argv)):
+        if i > 0:
+            print("{}: {}".format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-""" prints the sum of all arguments """
-arg_len = len(argv) - 1
-sum = 0
+if __name__ == "__main__":
+    from sys import argv
+    """ prints the sum of all arguments """
+    arg_len = len(argv) - 1
+    sum = 0
 
-if arg_len > 0:
-    for i in range(arg_len):
-        sum += int(argv[i + 1])
+    if arg_len > 0:
+        for i in range(arg_len):
+            sum += int(argv[i + 1])
 
-print("{}".format(sum))
+    print("{}".format(sum))
