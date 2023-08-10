bb1983c144c2c30ae007baa6265fd48ba7fca5b0
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-08-10 19:53:50 +0000

    add files

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+arg_len = len(argv) - 1
+if arg_len == 1:
+    print("{} argument:".format(arg_len))
+elif arg_len > 2:
+    print("{} arguments:".format(arg_len))
+else:
+    print("{} arguments.".format(0))
+
+for i in range(len(argv)):
+    if i > 0:
+        print("{}: {}".format(i, argv[i]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+""" prints the sum of all arguments """
+arg_len = len(argv) - 1
+sum = 0
+
+if arg_len > 0:
+    for i in range(arg_len):
+        sum += int(argv[i + 1])
+
+print("{}".format(sum))
