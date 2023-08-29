595d6126e251be47a0eedafbe8b2fd176bc9a26e
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-08-29 21:36:52 +0000

    add file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    square class
+"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
