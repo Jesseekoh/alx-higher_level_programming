a06495d457246d56c24840ed46af2fc7866202ed
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-09-05 00:32:50 +0000

    add 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -37,3 +37,9 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return (self.__width * 2) + (self.__height * 2)
