95760836ad9cf100e2bcac3bab4619ab4c243f2f
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-09-06 01:34:58 +0000

    add 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+4-print_square
+This module supplies one function, print_square().
+"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
