743dfc0995bb77d383569cb8d63383e7840994c5
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-09-06 02:38:02 +0000

    add files

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -1,6 +1,12 @@
-def increment(n):
-    n += 1
+copy_list = __import__('19-copy_list').copy_list
 
-a = 1
-increment(a)
-print(a)
+my_list = [1, 2, 3]
+print(my_list)
+
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+print(new_list is my_list)
