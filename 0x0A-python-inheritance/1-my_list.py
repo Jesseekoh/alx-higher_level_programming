6c106463187722b951c6dce7008d3b1098d24b42
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-09-11 16:22:03 +0000

    fix 3: task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,5 +11,5 @@ class MyList(list):
         list (_type_): _description_
     """
     def print_sorted(self):
-        sorted_list = self.copy()
+        sorted_list = super().copy()
         print(sorted(sorted_list))
