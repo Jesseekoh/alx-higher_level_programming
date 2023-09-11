6f69433f7599e5e68a6e9704faf30a82da03c681
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-09-11 15:14:23 +0000

    add files

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    MyList class
+"""
+
+
+class MyList(list):
+    """Prints out a sorted list
+
+    Args:
+        list (_type_): _description_
+    """
+    def print_sorted(self):
+        print(sorted(self))
