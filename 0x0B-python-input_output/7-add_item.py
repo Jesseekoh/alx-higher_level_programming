be51381cd955ff39962497efb320e2affaa82fbd
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-09-12 23:43:37 +0000

    add task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from sys import argv
+"""7-add_items module"""
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_items():
+    filename = "add_item.json"
+    try:
+        my_arr = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_arr = []
+    for i in range(1, len(argv)):
+        my_arr.append(argv[i])
+    save_to_json_file(my_arr, filename)
+
+
+add_items()
