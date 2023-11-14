4227b6287d9bfbd5865f32a5f56116e06d984491
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-11-14 13:06:21 +0100

    task 11: fix 1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-
-if (argv.length <= 3) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let max = argv[2];
-  for (let i = 2; i < argv.length; i++) {
-    if (max < argv[i]) {
-      max = argv[i];
-    }
-  }
-  console.log(max);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
