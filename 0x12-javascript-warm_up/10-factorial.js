c54350543b7437f44dbc84dcb5aa631819857dc4
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-11-14 11:16:24 +0100

    task 10: add file

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+function factorial (a) {
+  const n = Number(a);
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * factorial(Number(a) - 1);
+}
+
+console.log(factorial(argv[2]));
