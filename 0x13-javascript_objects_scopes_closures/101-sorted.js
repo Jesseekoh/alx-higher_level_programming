bb99ed6fe6642c47c6bdff33bb6a3c15cb302cf2
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-11-15 11:12:52 +0100

    task 12: fix 4

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-
-const dict = require('./101-data').dict;
+const dict = require('./101-data.js').dict;
 const output = {};
 for (const entry in dict) {
   if (!output[dict[entry]]) output[dict[entry]] = [];
   output[dict[entry]].push(entry);
 }
 console.log(output);
+
