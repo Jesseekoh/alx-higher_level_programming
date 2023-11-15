6d2960c20b503876ac65d4ce56292fcfe647d2e3
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-11-15 03:05:10 +0100

    task 6: fix 1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,38 +1,8 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const buffer = this.height;
-    this.height = this.width;
-    this.width = buffer;
-  }
-
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c = 'X') {
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
     }
   }
 }
