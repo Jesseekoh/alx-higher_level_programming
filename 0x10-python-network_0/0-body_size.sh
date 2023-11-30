75d504854dbeadf5b0484374f8bba8b4143ff1e5
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-12-01 00:28:20 +0100

    task 2: add file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a request to a passed URL and displays the size of the body of 
-# the response
+# sends a request to a passed URL and displays the size of the body of the response
 curl -so /dev/null -w '%{size_download}\n' "$1"
