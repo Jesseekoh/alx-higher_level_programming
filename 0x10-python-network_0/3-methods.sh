a1cb47751f37657c6763c289aba13e5c6350efb2
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-12-01 10:47:26 +0100

    task 3: fix 1

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # a bash script that display all HTTP methods the server will accept
-allowed_methods=$(curl -s -I -X OPTIONS <server_url> | grep -i "Allow" | cut -d' ' -f2-)
-echo "Allowed HTTP methods: $allowed_methods"
+curl -sI "$1" | grep "Allow" | cut -d' ' -f2-
