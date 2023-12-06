3b7927d343fe2b3acfe2cde7e455e74bbe769b4c
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-12-06 05:48:23 +0100

    task 4: fix 3

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-#  a Bash script that takes in a URL and displays the body of the response
+#a Bash script that takes in a URL and displays the body of the response
 curl -sX GET -H "X-School-User-Id: 98" "$1"
