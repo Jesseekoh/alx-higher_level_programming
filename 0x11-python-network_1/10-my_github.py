bc15d9bec0cdc46fd97c1304afe035857e7d9bb7
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-12-06 05:03:54 +0100

    task 9: fix 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,27 +2,10 @@
 """ a Python script that takes your GitHub credentials
 (username and password) and uses the GitHub API to display your id"""
 
-import requests
-from sys import argv
 
-if __name__ == '__main__':
-
-    # Create a session
-    session = requests.Session()
-    username = argv[1]
-    password = argv[2]
-    # Set the username and password for authentication
-    session.auth = (username, password)
-
-    # Make a GET request to the GitHub API to get the user's information
-    response = session.get('https://api.github.com/user')
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Get the user's id from the response
-        user_id = response.json()['id']
-
-        # Display the user's id
-        print(f"GitHub ID: {user_id}")
-    else:
-        print("Failed to retrieve GitHub ID")
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+    auth = (argv[1], argv[2])
+    request = requests.get("https://api.github.com/user", auth=auth)
+    print(request.json().get("id"))
